fe69b06ca1f14c6cad0220e1fbcf928c534526e5
sindreruud <sindreruud@noreply.com>
2025-03-27 14:37:04 +0100

    debugging

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -356,15 +356,15 @@ class DiscogsPlugin(BeetsPlugin):
 
         # Adapting for discogs key="value" format and adding to query
         filters_str = ', '.join(f'{key}="{value}"' for key, value in query_filters.items())
-        if filters_str:
-            query = f"{query}, {filters_str}"
+        #if filters_str:
+        #    query = f"{query}, {filters_str}"
 
         # debug logging
         self._log.debug("(get_albums) filters_str: {}", filters_str)
-        self._log.debug("(get_albums) Final query: {}", query)
+        #self._log.debug("(get_albums) Final query: {}", query)
 
         try:
-            releases = self.discogs_client.search(query, type="release").page(1)
+            releases = self.discogs_client.search(query, filters_str, type="release").page(1)
 
         except CONNECTION_ERRORS:
             self._log.debug(
